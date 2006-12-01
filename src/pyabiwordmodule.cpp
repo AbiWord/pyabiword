0d1bb10f21f265fad017ec1249fb1f23f4a003d0
Rob Staudinger <robert.staudinger@gmail.com>
2006-12-01 10:21:14 +0000

    use new libabiword initialization
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/pyabiword/trunk@21180 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/pyabiwordmodule.cpp
+++ src/pyabiwordmodule.cpp
@@ -9,27 +9,21 @@
 
 /* include this first, before NO_IMPORT_PYGOBJECT is defined */
 #include <pygobject.h>
-#include <ap_Args.h>
-#include <ap_UnixApp.h>
+#include <libabiword.h>
 
 extern "C" {
 
 extern void pyabiword_register_classes (PyObject *d);
 extern PyMethodDef pyabiword_functions[];
 
-static AP_UnixApp *_abiword_app = NULL;
-
 DL_EXPORT(void)
 initabiword (void)
 {
     PyObject *m, *d;
+    const char *argv[] = { PACKAGE, NULL };
 
     init_pygobject ();
-
-    XAP_Args XArgs = XAP_Args(PACKAGE);
-    _abiword_app = new AP_UnixApp(&XArgs, PACKAGE);
-    AP_Args Args = AP_Args(&XArgs, PACKAGE, _abiword_app);
-    Args.parsePoptOpts();
+    libabiword_init (1, argv);
 
     m = Py_InitModule ("abiword", pyabiword_functions);
     d = PyModule_GetDict (m);
@@ -41,4 +35,4 @@ initabiword (void)
     }
 }
 
-};
+}; // extern "C"
