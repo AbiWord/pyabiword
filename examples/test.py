23b9a2cccb71a06a0cf25982986cd62b9fc751d0
Martin Sevior <martines@unimelb.edu.au>
2009-02-25 12:07:40 +0000

    Backport of tomeu's enahncements to the python bindings for abiwidget screenshots.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/pyabiword/branches/ABI-2-6-0-STABLE@25747 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- examples/test.py
+++ examples/test.py
@@ -6,15 +6,33 @@ pygtk.require('2.0')
 import gtk
 import abiword
 
-window = gtk.Window()
+window = gtk.Window(type=gtk.WINDOW_TOPLEVEL)
 window.set_default_size(640, 480)
+window.resize(640,480)
+window.show()
 window.connect('delete-event', gtk.main_quit)
 
+box = gtk.VBox()
+window.add(box)
+box.show()
+
 abi = abiword.Canvas()
-window.add(abi)
+box.add(abi)
+abi.show()
+
+window.show()
+
+b = gtk.Button('render page')
+box.add(b)
+b.show()
+
+i = gtk.Image()
+box.add(i)
+i.show()
+
+def _clicked_cb(widget, abi, i):
+    i.props.pixbuf = abi.render_page_to_image(0)
+
+b.connect('clicked', lambda widget: _clicked_cb(widget, abi, i))
 window.show_all()
-
-abi.set_property("map-to-screen", True)
-abi.set_property("load-file", "test.abw")
-
 gtk.main()
